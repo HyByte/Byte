a79f9a9ca6ea51da9368226ed8697e8d7f8c140d
HyByte <talkdrove@gmail.com>
2024-05-26 11:47:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer-core');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const os = require('os');
 const { exec } = require('child_process');
